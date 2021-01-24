f00ba23c7f130c277800353ca83d1fa9efd59797
dominicsimon <dominicsimon@live.com>
2021-01-24 09:20:40 +0300

    text boxes and button done

---
+++ CryptoPad/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox2.Text = TextBox1.Text
+        TextBox2.Refresh()
+    End Sub
+End Class
